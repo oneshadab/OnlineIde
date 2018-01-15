467537eb3aa7e7e35234453a5a5d96f2e30d0ff9
Shadab <oneshadab@gmail.com>
2018-01-15 20:36:42 +0600

    testtttt

--- OnlineIde/settings.py
+++ OnlineIde/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wo6z#$(l_*+8v(31^up87y%=a_w%pvhy4&(h33i#n)t^*0(1f_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onlineide123.herokuapp.com']
 
 
 # Application definition
